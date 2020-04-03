222dc8f34bc58ca941ca16d731a5a145c41be279
Brian Jackson <bjack205@gmail.com>
2020-04-03 12:21:09 -0700

    Add packages script

---
+++ add_packages.jl
@@ -0,0 +1,6 @@
+import Pkg
+Pkg.add(PackageSpec("Rotations", url="https://github.com/bjack205/Rotations.jl.git"))
+Pkg.add(PackageSpec("RobotDynamics", url="https://github.com/RoboticExplorationLab/RobotDynamics.jl.git"))
+Pkg.add(PackageSpec("TrajOptCore", url="https://github.com/RoboticExplorationLab/TrajOptCore.jl.git"))
+Pkg.add(PackageSpec("TrajectoryOptimization", rev="split"))
+Pkg.add(PackageSpec("TrajOptPlots", url="https://github.com/RoboticExplorationLab/TrajOptPlots.jl.git"))
