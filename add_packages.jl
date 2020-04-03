9f14aa0b444e6e0cb01135845a76241485cb78d6
Brian Jackson <bjack205@gmail.com>
2020-04-03 12:27:42 -0700

    Add robot zoo and fix pkgspecs

--- add_packages.jl
+++ add_packages.jl
@@ -1,6 +1,7 @@
 import Pkg
-Pkg.add(PackageSpec("Rotations", url="https://github.com/bjack205/Rotations.jl.git"))
-Pkg.add(PackageSpec("RobotDynamics", url="https://github.com/RoboticExplorationLab/RobotDynamics.jl.git"))
-Pkg.add(PackageSpec("TrajOptCore", url="https://github.com/RoboticExplorationLab/TrajOptCore.jl.git"))
-Pkg.add(PackageSpec("TrajectoryOptimization", rev="split"))
-Pkg.add(PackageSpec("TrajOptPlots", url="https://github.com/RoboticExplorationLab/TrajOptPlots.jl.git"))
+Pkg.add(PackageSpec(name="Rotations", url="https://github.com/bjack205/Rotations.jl.git"))
+Pkg.add(PackageSpec(name="RobotDynamics", url="https://github.com/RoboticExplorationLab/RobotDynamics.jl.git"))
+Pkg.add(PackageSpec(name="RobotZoo", url="https://github.com/bjack205/RobotZoo.jl.git"))
+Pkg.add(PackageSpec(name="TrajOptCore", url="https://github.com/RoboticExplorationLab/TrajOptCore.jl.git"))
+Pkg.add(PackageSpec(name="TrajectoryOptimization", rev="split"))
+Pkg.add(PackageSpec(name="TrajOptPlots", url="https://github.com/RoboticExplorationLab/TrajOptPlots.jl.git"))
